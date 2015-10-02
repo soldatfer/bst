9b8b8e31067f68879f4c4d5f0ce5f0624a73cf1d
Soldatfer <soldatfer@gmail.com>
2015-10-02 21:39:20 +0530

    add init code

---
+++ bst.py
@@ -0,0 +1,43 @@
+class BinarySearchTree():
+
+    def __init__(self, key=None, value=None):
+        """ Initialize the tree.
+
+        If no key provided -> nothing
+        else -> create a new node for the root
+        """
+
+        if key == None:
+            self.root = None
+        else:
+            self.root = TreeNode(key, value)
+
+    def inorder(self):
+        """ Return the bst inorder. """
+
+        if self.root == None:
+            return []
+        else:
+            return self.root.inorder()
+
+class TreeNode():
+
+    def __init__(self, key, value):
+        """ Create a new tree node. 
+        
+        Key has to be non None.
+        """
+
+        self.key = key
+        self.value = value
+        self.leftChild = None
+        self.rightChild = None
+
+    def inorder(self):
+        """ Return the in order key, value pairs. """
+        ans = []
+        if self.leftChild != None:
+            ans += self.leftChild.inorder()
+        ans.append((key, value))
+        if self.rightChild != None:
+            ans += self.rightChild.inorder()
